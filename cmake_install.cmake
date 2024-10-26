f4d3b18020ccc968ee9d3bec39bb4dee17aac0fb
Oghenemarho Orukele <orukele.dev@gmail.com>
2024-10-26 09:59:14 +0200

    added encode and decode lenght function

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -34,7 +34,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/oghenemarho/Developer/ówe/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/oghenemarho/Developer/ówe/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- src/mqtt.c
+++ src/mqtt.c
@@ -30,15 +30,37 @@ int mqtt_encode_length(unsigned char *buf, size_t len)
     int bytes = 0;
     do
     {
-        if (bytes + 1 > MAX_LEN_BYTES)
+        if (bytes + 1 > MAX_LEN_BYTES) // check if maximum allowed bytes have been exceeded
             return bytes;
-        short d = len % 128;
-        len /= 128;
+        short d = len % 128; // Get lowest 7 bits of the lenght
+        len /= 128;          // Divide lenght by 128 for next iteration
         /* if there are more digits to encode, set the top bit of this digit */
         if (len > 0)
-            d |= 128;
-        buf[bytes++] = d;
+            d |= 128;     // Set MSB to 1 (128 in binary: 1000_0000)
+        buf[bytes++] = d; // Store the encoded byte
     } while (len > 0);
 
     return bytes;
+}
+
+/*
+ * Decode Remaining Lenght comprised of Variable Header and Payload if present.
+ * It does not take into account the bytes for storing lenght. Refer to MQTT v3.1.1
+ * algorithm for implementation suggestions.
+ *
+ * TODO Handle cases were multiplier > 128 * 128 * 128
+ */
+unsigned long long mqtt_decode_length(const unsigned char **buf)
+{
+    char c;
+    int multiplier = 1;
+    unsigned long long value = 0LL;
+    do
+    {
+        c = **buf;                       // Get byte from buffer
+        value += (c & 127) * multiplier; // Add 7 bits to value
+        multiplier *= 128;               // Increase multiplier for next byte
+        (*buf)++;                        // Move to next byte
+    } while ((c & 128) != 0); // Continue if MSB is 1
+    return value;
 }
